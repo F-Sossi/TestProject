9fcc359acd0c06aef10e31aeffa5797308b6bc10
Frank Sossi <frank.sossi@gmail.com>
2022-12-30 07:41:33 +0000

    Edited names and loop construct to be more pythonic

--- main.py
+++ main.py
@@ -2,13 +2,20 @@
 # calculates the minimum number of moves required to reach the end of a list
 
 def minimum_moves(number_list):
-    # Initialize an array to store the minimum number of moves required to reach each position
-    dp = [float('inf')] * len(number_list)
+    """Dynamic method to calculate the minimum number of moves required 
+    to reach the end of the list"""
+
+    # check if empty list
+    if not number_list:
+        return -1
+    # Initialize an array to store the minimum number of moves required to 
+    # reach each position
+    distance_table = [float('inf')] * len(number_list)
     # The minimum number of moves required to reach the starting position is 0
-    dp[0] = 0
+    distance_table[0] = 0
 
     # Loop through all the positions in the list
-    for i in range(len(number_list)):
+    for i,number in enumerate(number_list):
         # Loop through all the possible positions that can be reached
         # from the current position
         for j in range(1, number_list[i] + 1):
@@ -19,16 +26,16 @@ def minimum_moves(number_list):
             if next_position >= len(number_list):
                 continue
 
-            # Update the minimum number of moves required to reach the next position
-            dp[next_position] = min(dp[next_position], dp[i] + 1)
+            # Update the minimum number of moves required to reach the next
+            distance_table[next_position] = min(distance_table[next_position], distance_table[i] + 1)
 
-    min_moves = dp[-1]
+    min_moves = distance_table[-1]
 
     # if min moves is infinity, then the end of the list is unreachable return -1
     if min_moves == float('inf'):
         return -1
-    else:
-        return min_moves
+    
+    return min_moves
 
 
 # Test the function
@@ -36,7 +43,7 @@ print(minimum_moves([3, 2, 1, 4, 2, 3, 1, 1, 3]))  # Output: 3
 print(minimum_moves([1, 2, 3]))  # Output: 2
 print(minimum_moves([3, 2, 1]))  # Output: 1
 print(minimum_moves([1, 1, 0, 1]))  # Output: 3
-print(minimum_moves([1, 1]))  # Output: 3
+print(minimum_moves([ ]))  # Output: 3
 
 
 
